baf46d531e64c11c73ccbda3a4ef5fe0055c355a
care1e55 <sfilms89@gmail.com>
2020-12-01 18:05:00 +0300

    next_perm

---
+++ next_perm.py
@@ -0,0 +1,18 @@
+def next_permutation(string):
+    for i in range(len(string)-2, -1, -1):
+        if string[i] < string[i+1]:
+            pos = i + 1
+            for j in range(i+1, len(string)):
+                if (string[j] > string[i]) and (string[j] < string[pos]):
+                    pos = j
+            string[i], string[pos] = string[pos], string[i]
+            string[i+1:] = string[:i:-1]
+            return "".join(string)
+    return "".join(string[::-1])
+
+while True:
+    try:
+        string=input()
+        print(next_permutation(list(string)))
+    except EOFError:
+        break
